5c29c93e236f748b1769abcbe66f517b9c1fb05d
Vitor Carrilho <vitor.carrilho@hotmail.com>
2020-03-05 01:30:39 -0300

    rotas autenticadas

--- GRTSDigital/routes/web.php
+++ GRTSDigital/routes/web.php
@@ -12,28 +12,28 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', 'HomeController@index')->name('home');
-
-Route::get('/clientes', 'ClienteController@index')->name('clientes');
-Route::get('/cliente/{id}', 'ClienteController@cliente')->name('cliente');
-Route::get('formulario/cliente/cadastrar/editar/{id?}', 'ClienteController@formularioCadastrarEditar')->name('formulario_cliente_cadastrar_editar');
-Route::post('cadastrar/cliente','ClienteController@cadastrarCliente')->name('cadastrar_cliente');
-Route::post('editar/cliente','ClienteController@editarCliente')->name('editar_cliente');
-Route::post('remover/cliente','ClienteController@removerCliente')->name('remover_cliente');
-
-
-Route::get('/usuarios', 'UsuarioController@index')->name('usuarios');
-Route::get('formulario/usuario/cadastrar/editar/{id?}', 'UsuarioController@formularioCadastrarEditar')->name('formulario_usuario_cadastrar_editar');
-Route::post('cadastrar/usuario','UsuarioController@cadastrarUsuario')->name('cadastrar_usuario');
-Route::post('editar/usuario','UsuarioController@editarUsuario')->name('editar_usuario');
-Route::post('remover/usuario','UsuarioController@removerUsuario')->name('remover_usuario');
-
 
 Auth::routes(['register' => false]);
 Route::get('/logout', 'Auth\LoginController@logout');
 Route::group([ 'middleware' => 'auth'], function()
 {
 
-    //Route::get('/', 'HomeController@index')->name('home');
+    Route::get('/', 'HomeController@index')->name('home');
+
+    Route::get('/clientes', 'ClienteController@index')->name('clientes');
+    Route::get('/cliente/{id}', 'ClienteController@cliente')->name('cliente');
+    Route::get('formulario/cliente/cadastrar/editar/{id?}', 'ClienteController@formularioCadastrarEditar')->name('formulario_cliente_cadastrar_editar');
+    Route::post('cadastrar/cliente','ClienteController@cadastrarCliente')->name('cadastrar_cliente');
+    Route::post('editar/cliente','ClienteController@editarCliente')->name('editar_cliente');
+    Route::post('remover/cliente','ClienteController@removerCliente')->name('remover_cliente');
+
+
+    Route::get('/usuarios', 'UsuarioController@index')->name('usuarios');
+    Route::get('formulario/usuario/cadastrar/editar/{id?}', 'UsuarioController@formularioCadastrarEditar')->name('formulario_usuario_cadastrar_editar');
+    Route::post('cadastrar/usuario','UsuarioController@cadastrarUsuario')->name('cadastrar_usuario');
+    Route::post('editar/usuario','UsuarioController@editarUsuario')->name('editar_usuario');
+    Route::post('remover/usuario','UsuarioController@removerUsuario')->name('remover_usuario');
+
+
 
 });
